26c62ca35ee6c53e81bac21eea167b3f0d5088fd
robgleeson <rob@flowof.info>
2011-09-10 22:17:22 +0100

    Extend test suite.

--- spec/minitest_context_spec.rb
+++ spec/minitest_context_spec.rb
@@ -8,5 +8,26 @@ describe MiniTest::Context do
       @example.must_be_instance_of(Array)
     end
   end
-end
 
+  describe "a suite which has not inherited a context." do
+    it "should not inherit any state from suites with inherited contexts." do
+      @example.must_be_instance_of(NilClass)
+    end
+  end
+
+  describe "two suites who inherit the same context." do
+    inherit_context :before_block
+    
+    describe "Suite 1" do
+      it "should inherit context from the parent suite." do
+        @example.must_be_instance_of(Array)
+      end
+    end
+
+    describe "Suite 2" do
+      it "should inherit context from the parent suite." do
+        @example.must_be_instance_of(Array)
+      end
+    end
+  end
+end
