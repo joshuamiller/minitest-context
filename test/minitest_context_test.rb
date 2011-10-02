21568a7156c215f3fec228c8e71e5fbbc9c40b65
Rob Gleeson <rob@flowof.info>
2011-10-02 07:24:26 +0100

    Swap "should" in test descriptions with "must".
    The minitest/spec DSL is built on top of .must_* assertions.

--- test/minitest_context_test.rb
+++ test/minitest_context_test.rb
@@ -1,10 +1,10 @@
 require File.expand_path("setup.rb", File.dirname(__FILE__))
 
 describe "Contexts" do
-  describe "an inherited context." do
+  describe "An inherited context." do
     inherit_context :before_block
 
-    it "should execute a before block and assign an Array to @example." do
+    it "must assign an Array to @example." do
       @example.must_be_instance_of(Array)
     end
   end
@@ -13,13 +13,13 @@ describe "Contexts" do
     inherit_context :before_block
     
     describe "Suite 1" do
-      it "should inherit context from the parent suite." do
+      it "must the inherit context from the parent suite." do
         @example.must_be_instance_of(Array)
       end
     end
 
     describe "Suite 2" do
-      it "should inherit context from the parent suite." do
+      it "must the inherit context from the parent suite." do
         @example.must_be_instance_of(Array)
       end
     end
@@ -28,20 +28,20 @@ describe "Contexts" do
   describe "A stacked context." do
     inherit_context :stacked_context
 
-    it "should execute all stacked contexts." do
+    it "must execute all stacked contexts." do
       @number.must_equal(2)
     end
   end
 end
 
 describe "MiniTest::Context.define" do
-  it "should raise an ArgumentError when no block is recieved." do
+  it "must raise an ArgumentError when no block is recieved." do
     proc { MiniTest::Context.define(:fail) }.must_raise(ArgumentError)
   end
 end
 
 describe "MiniTest::Spec.inherit_context" do
-  it "should raise an ArgumentError when no context by given name can be found." do
+  it "must raise an ArgumentError when no context by given name can be found." do
     proc { MiniTest::Spec.inherit_context(:fail) }.must_raise(ArgumentError) 
   end
 end
